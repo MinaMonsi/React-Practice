88ff096efd4a4ec48dcc2e1d8733d2608eda746e
Marina Monsivais <minamonsi@me.com>
2022-02-15 22:19:58 -0600

    WIP

--- business-card/src/App.js
+++ business-card/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Bio from "./components/Bio";
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="container">
       <Header />
       <Bio />
+      <Footer />
     </div>
   );
 }
