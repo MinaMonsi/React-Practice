61a451c5b8c432f13cb92577ed85e55568313bca
Marina Monsivais <minamonsi@me.com>
2022-02-18 15:52:21 -0600

    Added social media logos

--- business-card/src/components/Footer.js
+++ business-card/src/components/Footer.js
@@ -1,9 +1,16 @@
 import React from "react";
+import Facebook from "../images/Facebook.png";
+import Twitter from "../images/Twitter.png";
+import Instagram from "../images/Instagram.png";
+import GitHub from "../images/GitHub.png";
 
 function Footer(){
     return(
         <footer>
-
+            <img className="logos" src={Twitter} alt="Twitter logo" />
+            <img className="logos" src={Facebook} alt="Facebook logo" />
+            <img className="logos" src={Instagram} alt="Instagram logo" />
+            <img className="logos" src={GitHub} alt="GitHub logo" />
         </footer>
     )
 }
