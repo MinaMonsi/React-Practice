d27b13d4f91ac1e3d7b9b44afa044a23f10b43ed
Marina Monsivais <minamonsi@me.com>
2022-02-26 16:50:25 -0600

    WIP

--- business-card/src/components/Footer.js
+++ business-card/src/components/Footer.js
@@ -7,10 +7,17 @@ import GitHub from "../images/GitHub.png";
 function Footer(){
     return(
         <footer>
+            <a href="https://twitter.com/minamonsi">
             <img className="logos" src={Twitter} alt="Twitter logo" />
+            </a>
+            <a href=" https://www.facebook.com/marina.perez.9028">
             <img className="logos" src={Facebook} alt="Facebook logo" />
+            </a>
+            <a href="#"></a>
             <img className="logos" src={Instagram} alt="Instagram logo" />
+            <a href="https://www.instagram.com/minamonsi/">
             <img className="logos" src={GitHub} alt="GitHub logo" />
+            </a>    
         </footer>
     )
 }
