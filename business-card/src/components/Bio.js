5047e53b2cf6960e7abbb59274d0bd2ef10886d9
Marina Monsivais <minamonsi@me.com>
2022-02-26 14:36:20 -0600

    WIP

--- business-card/src/components/Bio.js
+++ business-card/src/components/Bio.js
@@ -7,7 +7,7 @@ function Bio(){
             <p> I'm a frontend developer with an interest for design. I try to keep up with best practice and I'm always looking for ways to improve and increase my coding skills. </p> 
             
             <h1>Interests</h1>
-            <p>Food lover. Music scholar. Reader. Internet fanatic. Dancer. Travel geek. Hiking enthusiast. Vanilla Coke Coffee lover. </p>
+            <p>Food lover. Music scholar. Reader. Internet researcher. Dancer. Travel geek. Hiking enthusiast. Vanilla Coke Coffee lover. </p>
         </main>
     )
 }
